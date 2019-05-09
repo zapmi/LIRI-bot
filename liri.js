c6a9d690f01d0a2cd4233e9f1c34ca9bb86ec163
Mike Z <mzapata2991@gmail.com>
2019-05-08 20:10:37 -0500

    added spotify this song call

--- liri.js
+++ liri.js
@@ -1,8 +1,21 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+var Spotify = require('node-spotify-api');
 
 //spotify-this-song
+// node liri.js spotify-this-song '<song name here>'
+// var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify({
+    id: keys.spotify.id,
+    secret: keys.spotify.secret
+  });
+//   var input1 = process.argv[2];
 
-node liri.js spotify-this-song '<song name here>'
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+
+console.log(data); 
+});
