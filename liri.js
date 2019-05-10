959ac07870227d37b60d269cffd14d70c49a877c
zapmi <mzapata2991@gmail.com>
2019-05-10 10:27:57 -0500

    started sportify code over, still getting token missing error

--- liri.js
+++ liri.js
@@ -2,20 +2,25 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
+var spotify = new Spotify({
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+});
+
+var operation = process.argv[2];
+var trackTitle = process.argv[3];
 
 //spotify-this-song
 // node liri.js spotify-this-song '<song name here>'
-// var spotify = new Spotify(keys.spotify);
-var spotify = new Spotify({
-    id: keys.spotify.id,
-    secret: keys.spotify.secret
+var spotify = new Spotify(keys.spotify);
+
+if (operation == 'spotify-this-song'){
+  spotify.search({ type: 'track', query: trackTitle, limit: 5 }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+   
+  console.log(data); 
   });
-  var trackTitle = process.argv[3];
 
-spotify.search({ type: 'track', query: trackTitle, limit: 10 }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
-
-console.log(data); 
-});
+}
