d2b9ef08a24bc072be453753e8213f483951be87
Mike Z <mzapata2991@gmail.com>
2019-05-13 22:24:51 -0500

    spotify search almost done

--- liri.js
+++ liri.js
@@ -10,11 +10,11 @@ var userInput = process.argv.splice(3, process.argv.length).join(" ");
 
 if (operation == 'spotify-this-song') {
   spotifyThisSong();
-} else if (operation == 'concert-this'){
+} else if (operation == 'concert-this') {
   concertThis();
-} else if (operation == 'movie-this'){
+} else if (operation == 'movie-this') {
   movieThis();
-} else if (operation == 'do-what-it-says'){
+} else if (operation == 'do-what-it-says') {
   doWhatItSays();
 }
 
@@ -28,13 +28,23 @@ function concertThis() {
 //node liri.js spotify-this-song '<song name here>'
 function spotifyThisSong() {
   var song = userInput;
-  spotify.search({ type: 'track', query: song, offset: 1, limit: 1 }, function (err, data) {
+  spotify.search({ type: 'track', query: song, limit: 1 }, function (err, data) {
     if (err) {
-      return console.log('Error occurred: ' + err);
+      console.log('Error occurred: ' + err);
     }
-    console.log(JSON.stringify(data, null, 2));
-  });
+    else {
+      for (var i = 0; i < data.tracks.items.length; i++) {
+        console.log('Artist: ' + data.tracks.items[i].artists[0].name);
+        console.log('Song name: ' + data.tracks.items[i].name);
+        console.log('Preview: ' + data.tracks.items[i].preview_url);
+        console.log('Album: ' + data.tracks.items[i].album.name);
+        
 
+      }
+
+    }
+
+  });
 }
 
 //movie-this
